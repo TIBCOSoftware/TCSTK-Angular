2fedc0979255cfdd65a0210006994d2969e565e7
jezrsmith <jez.smith@live.com>
2018-12-07 09:12:45 +0000

    work on notes component

---
+++ src/app/pipes/duration-since.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { DurationSincePipe } from './duration-since.pipe';
+
+describe('DurationSincePipe', () => {
+  it('create an instance', () => {
+    const pipe = new DurationSincePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
