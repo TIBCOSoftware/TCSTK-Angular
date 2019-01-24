0d872f7eb13b3f5d25bd8a5dd441fa522e57ed79
jezrsmith <jez.smith@live.com>
2019-01-24 13:48:12 +0000

    more mocks. Handle mocked svg from webresources.

--- projects/tc-liveapps-lib/src/lib/interceptors/mocking-interceptor.ts
+++ projects/tc-liveapps-lib/src/lib/interceptors/mocking-interceptor.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpRequest, HttpResponse, HttpInterceptor, HttpHandler, HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, of, onErrorResumeNext} from 'rxjs';
-import {map, catchError} from 'rxjs/operators';
+import {map, catchError, delay} from 'rxjs/operators';
 
 @Injectable()
 export class MockingInterceptor implements HttpInterceptor {
@@ -19,23 +19,54 @@ export class MockingInterceptor implements HttpInterceptor {
     // save original request
     const originalReq = req.clone();
 
+    // dont mock anything from assets folder
+    const urlparts = req.url.split('.');
+    const pathparts = urlparts[0].split('/');
+
+    if (pathparts[1] === 'assets') {
+      return this.sendRequest(originalReq, next);
+    }
+
+
     // try and find a mock response
     const resource = originalReq.url.split('?')[0];
     const segments: string[] = resource.split('/');
     const seg2: string[] = resource.split('/');
     let path = this.MOCK_BASE_PATH;
-    segments.splice(1, segments.length).forEach((seg) => {
+    // ignore the http/url parts if present
+    let responseType = 'json';
+    if (segments[0] === 'http:' || segments[0] === 'https:') {
+      segments.splice(0, 3);
+      // we are getting a webresource (will only work for text based resources like svg)
+      if (segments[0] === 'webresource') {
+        responseType = 'text';
+      }
+    } else {
+      // just remove the blank part
+      segments.splice(0, 1);
+    }
+    segments.splice(0, segments.length).forEach((seg) => {
       path = path + '/' + seg;
     });
-    path = path + '.json';
+    if (responseType !== 'text') {
+      path = path + '.json';
+    } else {
+      path = path + '.txt';
+    }
 
+    let options = {};
+    if (responseType === 'text') {
+      options = { responseType: 'text' };
+    }
     const httpRequest = new HttpRequest(
       <any>'GET',
-      path
+      path,
+      options
     );
     req = Object.assign(req, httpRequest);
     req = req.clone();
     return next.handle(req).pipe(
+      delay(100),
       map(
         data => {
           const d = <HttpResponse<any>> data;
