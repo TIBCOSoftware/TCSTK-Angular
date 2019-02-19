0b4168d6843b39970a8514556f35e84d2573bc38
jezrsmith <jez.smith@live.com>
2019-02-19 17:03:03 +0000

    Docs around resolver parameters

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -5,7 +5,7 @@ import {Claim} from 'tc-liveapps-lib';
 
 @Component({
   selector: 'laapp-home',
-  templateUrl: './home.component.html',
+  templateUrl: './templates/home.component.liveapps.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
