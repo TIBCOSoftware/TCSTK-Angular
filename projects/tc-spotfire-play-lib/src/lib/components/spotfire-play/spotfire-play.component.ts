2f95247fe86a01e84bf0bb0de3de7a1fadeb940f
hpeters83 <hpeters@tibco.com>
2019-03-11 11:55:49 +0100

    Table Max Numbers

--- projects/tc-spotfire-play-lib/src/lib/components/spotfire-play/spotfire-play.component.ts
+++ projects/tc-spotfire-play-lib/src/lib/components/spotfire-play/spotfire-play.component.ts
@@ -1,13 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {MatTableDataSource} from '@angular/material';
-import { DataSource } from '@angular/cdk/table';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 
 @Component({
   selector: 'tcsp-spotfire-play',
@@ -25,19 +17,9 @@ export class SpotfirePlayComponent implements OnInit {
   markingDataTables = new Array();
   markingHeaders = new Array(new Array());
   markingData = new Array(new Array(new Array()));
-  dataSource: PeriodicElement[] = [
-    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-  ];
-  displayedColumns = ['position', 'name', 'weight', 'symbol'];
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   constructor() { }
 
@@ -56,25 +38,29 @@ export class SpotfirePlayComponent implements OnInit {
     this.markingHeaders = new Array();
     this.markingData = new Array();
 
-    for (const key in data) {
-      if (data.hasOwnProperty(key)) {
-        console.log(key + ' -> ' + data[key]);
-        this.markingTitles.push(key);
-        const mData = new Array();
-        const markingElements = data[key];
-        for (const ekey in markingElements) {
-          console.log('ekey: ' + ekey);
-          this.markingDataTables.push(ekey);
+    // this.markingHeaders.push('Number');
+
+    // iterate over the marking data tables
+    for (const markingName in data) {
+      if (data.hasOwnProperty(markingName)) {
+        console.log(markingName + ' -> ' + data[markingName]);
+        this.markingTitles.push(markingName);
+        // const mData = new Array();
+        const markingElements = data[markingName];
+        for (const dataTableName in markingElements) {
+          console.log('elementKey: ' + dataTableName);
+          this.markingDataTables.push(dataTableName);
 
 
           // console.log("ekey data: " , data[ekey]);
           console.log('me data: ' , markingElements);
-          console.log('has property: ' + markingElements.hasOwnProperty(ekey));
-          if (markingElements.hasOwnProperty(ekey)) {
+          console.log('has property: ' + markingElements.hasOwnProperty(dataTableName));
+          if (markingElements.hasOwnProperty(dataTableName)) {
             const mDataRow = new Array();
-            console.log('mkey: ' , markingElements[ekey]);
-            const mkey = markingElements[ekey];
+            console.log('mkey: ' , markingElements[dataTableName]);
+            const mkey = markingElements[dataTableName];
             const headA = new Array();
+            headA.push('Number');
             for (const head in mkey) {
               console.log('head: ' + head);
               headA.push(head);
@@ -97,17 +83,20 @@ export class SpotfirePlayComponent implements OnInit {
             }
             console.log('mDataRow: ', mDataRow);
 
-            mData.push(this.transposeArray(mDataRow, mDataRow.length));
-            this.markingData.push(this.transposeArray(mDataRow, mDataRow.length));
-            /*
-            var results = {};
-            data.forEach(function(el) {
-              var name = el[0];
-              var prop = el[1];
-              var value = el[2];
-              results[name] = results[name] || {};
-              results[name][prop] = value;
-            })*/
+            // mData.push(this.transposeArray(mDataRow, mDataRow.length));
+            // Add numbers for the first column
+            const numberOfDataRows = mDataRow[0].length;
+            let rowNumberArray = new Array();
+            for (let i = 0 ; i < numberOfDataRows ; i++) {
+              rowNumberArray.push(i + 1);
+            }
+            // insert rows before
+            mDataRow.unshift(rowNumberArray);
+
+            this.markingData.push(this.transposeArray(mDataRow, numberOfDataRows));
+            this.dataSource = this.convertToJSON(this.markingData[this.markingData.length - 1]);
+            console.log("Datasource: " , this.dataSource);
+
 
             this.markingHeaders.push(headA);
 
@@ -118,46 +107,65 @@ export class SpotfirePlayComponent implements OnInit {
 
 
 
-        //this.markingData.push(mData);
+        // this.markingData.push(mData);
       }
     }
 
     console.log('Marking Headers: ' , this.markingHeaders);
     console.log('Marking Data: ', this.markingData);
-    // pivot table
-    /*
-    for (var pTable of this.markingData){
-      console.log("Before: " , pTable);
-      pTable.map((col, i) => pTable.map(row => row[i]));
-      console.log("After: " , pTable);
-      console.log("Lenght: " + pTable[0].length);
-      pTable = this.transposeArray(pTable[0], pTable[0].length);
-      console.log("After2: " , pTable);
-    }
 
-    var test = [
-      [1,2,3,4],
-      [1,2,3,4],
-      [1,2,3,4],
-    ];
-
-    console.log(this.transposeArray(test,4));
-*/
   }
 
-  private transposeArray(array, arrayLength){
-    var newArray = [];
-    for(var i = 0; i < arrayLength; i++){
-      newArray.push([]);
-    };
+  private convertToJSON(array) {
+    var objArray = [];
+    for (var i = 1; i < array.length; i++) {
+      objArray[i - 1] = {};
+      for (var k = 0; k < array[0].length && k < array[i].length; k++) {
+        var key = array[0][k];
+        objArray[i - 1][key] = array[i][k]
+      }
+    }
 
-    for(var i = 0; i < array.length; i++){
-      for(var j = 0; j < arrayLength; j++){
+    return objArray;
+  }
+
+  private transposeArray(array, arrayLength) {
+    console.log('transposing array) arrayLength: ' + arrayLength );
+    const newArray = [];
+    for (let i = 0; i < arrayLength; i++) {
+      newArray.push([]);
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < arrayLength; j++) {
         newArray[j].push(array[i][j]);
-      };
-    };
+      }
+    }
 
     return newArray;
   }
 
 }
+
+var ELEMENT_DATA = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
