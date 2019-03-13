624c4df790400fb13754bbd2228e4a0de3c261db
NamOsTibco <nthat@tibco.com>
2019-03-13 18:00:54 +0100

    Merge branch 'master' of https://github.com/TIBCOSoftware/cloud-starters-base
    # Conflicts:
    #	projects/tc-process-discovery-lib/src/lib/tc-process-discovery-lib.module.ts
    #	projects/tc-process-discovery-lib/src/public_api.ts

---
+++ projects/tc-check-workflow-monitor-lib/src/lib/components/select-service-display/select-service-display.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {ServiceDetails} from '../../models/service-details';
+
+
+@Component({
+  selector: 'tccwm-select-service-display',
+  templateUrl: './select-service-display.component.html',
+  styleUrls: ['./select-service-display.component.css']
+})
+export class SelectServiceDisplayComponent implements OnInit {
+
+  @Input()  serviceDetailsList: Array<ServiceDetails>;
+
+
+  @Output() selectServiceEvent: EventEmitter<ServiceDetails> = new EventEmitter<ServiceDetails>();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+  public selectService = (serviceDetail) => {
+    this.selectServiceEvent.emit(serviceDetail);
+
+
+  }
+
+
+}
