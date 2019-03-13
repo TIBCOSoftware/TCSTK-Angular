b702d7c32384de6a4d9783fd7c1a901682fb1820
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuels-MacBook-Pro.local>
2019-03-13 17:30:31 +0100

    It is possible change the datasource from widget.

--- projects/tc-process-discovery-lib/src/lib/components/pd-home/pd-home.component.ts
+++ projects/tc-process-discovery-lib/src/lib/components/pd-home/pd-home.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ToolbarButton, TcButtonsHelperService, GeneralConfig } from 'tc-core-lib';
 import { LiveAppsConfig, Claim, CaseType, RouteAction, CaseRoute } from 'tc-liveapps-lib';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
     selector: 'tcpd-pd-home',
@@ -15,12 +16,17 @@ export class PdHomeComponent implements OnInit {
     private claims: Claim;
     public sandboxId: number;
     // public selectedAppConfig: CaseType;
-    public userName: string;
+    public datasource: string;
     public toolbarButtons: ToolbarButton[];
     public viewButtons: ToolbarButton[];
     public applicationIds = [];
 
-    constructor(private router: Router, private buttonsHelper: TcButtonsHelperService, private route: ActivatedRoute) { }
+    constructor(
+        private router: Router, 
+        private buttonsHelper: TcButtonsHelperService, 
+        private route: ActivatedRoute,
+        public dialog: MatDialog
+    ) { }
 
     handleRouteAction = (routeAction: any) => {
         if (routeAction === 'caseClicked') {
@@ -28,10 +34,15 @@ export class PdHomeComponent implements OnInit {
             // case clicked - navigate to case - note need to pass appId and caseId
             this.router.navigate(['/starterApp/case/' + caseRoute.appId + '/' + caseRoute.typeId + '/' + caseRoute.caseRef]);
         }
+
         if (routeAction === 'config') {
             console.log('Config button clicked');
             this.router.navigate(['/starterApp/settings/general-application-settings']);
             // route to config page
+        }
+
+        if (routeAction === 'changedatasource'){
+            this.openDialog();
         }
 
         if (routeAction.value === "Process Mining View"){
@@ -41,12 +52,14 @@ export class PdHomeComponent implements OnInit {
         if (routeAction.value === "Case View"){
             this.router.navigate(['/starterApp/pd/case-view']);
         }
+
     }
 
     protected createToolbarButtons = (): ToolbarButton[] => {
-        const configButton = this.buttonsHelper.createButton('config', 'tcs-config-icon', true, 'Config', true, true);
+        const configButton = this.buttonsHelper.createButton('config', 'tcs-capabilities', true, 'Config', true, true);
         const refreshButton = this.buttonsHelper.createButton('refresh', 'tcs-refresh-icon', true, 'Refresh', true, true);
-        const buttons = [ configButton, refreshButton ];
+        const changeDatasource = this.buttonsHelper.createButton('changedatasource', 'tcs-config-icon', true, 'Change datasource', true, true);
+        const buttons = [ configButton, refreshButton, changeDatasource ];
         return buttons;
     }
     
@@ -62,13 +75,39 @@ export class PdHomeComponent implements OnInit {
         this.liveAppsConfig = this.route.snapshot.data.laConfigHolder.liveAppsConfig;
         this.claims = this.route.snapshot.data.claims;
         this.sandboxId = this.route.snapshot.data.claims.primaryProductionSandbox.id;
-        this.userName = this.claims.firstName + ' ' + this.claims.lastName;
+        this.datasource = 'DIS_000002';
         //   this.email = this.claims.email;
         //   this.userId = this.claims.id;
         this.toolbarButtons = this.createToolbarButtons();
         this.viewButtons = this.createViewButtons();
     }
 
+    animal: string;
+    name: string;
+  
+    openDialog = (): void => {
+        const dialogRef = this.dialog.open(PdChangeDatasourceDialog, {
+            width: '500px',
+            data: {datasource: this.datasource }
+          });
+      
+          dialogRef.afterClosed().subscribe(result => {
+            this.datasource = result;
+          });
+        }
+}
+
+export interface DialogData {
+  datasource: string;
+}
+@Component({
+  selector: 'pd-change-datasource-dialog',
+  templateUrl: 'pd-change-datasource-dialog.html',
+})
+export class PdChangeDatasourceDialog {
+
+  constructor(public dialogRef: MatDialogRef<PdChangeDatasourceDialog>, @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
 
 }
+
 
