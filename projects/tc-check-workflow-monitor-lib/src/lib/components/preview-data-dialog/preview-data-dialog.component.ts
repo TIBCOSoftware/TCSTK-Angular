3a04700b29bed9ed981179348e42019c71310e83
NamOsTibco <nthat@tibco.com>
2019-03-13 18:00:54 +0100

    Merge branch 'master' of https://github.com/TIBCOSoftware/cloud-starters-base
    # Conflicts:
    #	projects/tc-process-discovery-lib/src/lib/tc-process-discovery-lib.module.ts
    #	projects/tc-process-discovery-lib/src/public_api.ts

---
+++ projects/tc-check-workflow-monitor-lib/src/lib/components/preview-data-dialog/preview-data-dialog.component.ts
@@ -0,0 +1,34 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+
+@Component({
+  selector: 'tccwm-preview-data-dialog',
+  templateUrl: './preview-data-dialog.component.html',
+  styleUrls: ['./preview-data-dialog.component.css']
+})
+export class PreviewDataDialogComponent {
+
+
+  description: string;
+  jsonData: Array<any>;
+
+  constructor(
+    public dialogRef: MatDialogRef<PreviewDataDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data) {
+
+    this.description = data.description;
+    this.jsonData = data.jsonData;
+
+  }
+
+
+
+  save() {
+    this.dialogRef.close('save');
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+}
