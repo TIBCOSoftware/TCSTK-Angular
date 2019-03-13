52edc167591ceeee0107434571ce8c7475cbaedc
NamOsTibco <nthat@tibco.com>
2019-03-13 20:24:49 +0100

    changing to service

--- projects/tc-check-workflow-monitor-lib/src/lib/components/pd-home-page/pd-home-page.component.ts
+++ projects/tc-check-workflow-monitor-lib/src/lib/components/pd-home-page/pd-home-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {ServiceDetails} from '../../models/service-details';
+import {ServiceHandlerService} from '../../services/service-handler.service';
+import {map} from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material';
 
 
 
@@ -21,7 +24,7 @@ export class PdHomePageComponent implements OnInit {
   public serviceDetailsList = new Array();
 
 
-  constructor() {
+  constructor(private serviceHandler: ServiceHandlerService, private snackBar: MatSnackBar) {
 
 
 
@@ -38,25 +41,29 @@ export class PdHomePageComponent implements OnInit {
     // pick the first one
     this.curServiceDetails = serviceDetails;
 
-    serviceDetails = new ServiceDetails().deserialize({label: 'Create From BOT (TODO)', rootObjectName : 'mycases',
-      operation : '/CreateCasesFromBordereaux',
-      apiUrl: 'https://eu-west-1.integration.cloud.tibcoapps.com/zwwupj46ttb7alnauy7exvxwihssu2y3'});
-    this.serviceDetailsList.push(serviceDetails);
 
-    serviceDetails = new ServiceDetails().deserialize({label: 'Update From BPM (TODO)', rootObjectName : 'mycases',
-      operation : '/CreateCasesFromBordereaux',
+    serviceDetails = new ServiceDetails().deserialize({label: 'Update From Docapost (TODO)', rootObjectName : 'cases',
+      operation : '/UpdateLACasesFromDocapost',
       apiUrl: 'https://eu-west-1.integration.cloud.tibcoapps.com/zwwupj46ttb7alnauy7exvxwihssu2y3'});
     this.serviceDetailsList.push(serviceDetails);
 
 
-    serviceDetails = new ServiceDetails().deserialize({label: 'Update From Docapost (TODO)', rootObjectName : 'mycases',
+
+
+    serviceDetails = new ServiceDetails().deserialize({label: 'Create From BOT (TODO)', rootObjectName : 'cases',
+      operation : '/CreateCasesFromBordereaux',
+      apiUrl: 'https://eu-west-1.integration.cloud.tibcoapps.com/zwwupj46ttb7alnauy7exvxwihssu2y3'});
+    this.serviceDetailsList.push(serviceDetails);
+
+    serviceDetails = new ServiceDetails().deserialize({label: 'Update From BPM (TODO)', rootObjectName : 'cases',
       operation : '/CreateCasesFromBordereaux',
       apiUrl: 'https://eu-west-1.integration.cloud.tibcoapps.com/zwwupj46ttb7alnauy7exvxwihssu2y3'});
     this.serviceDetailsList.push(serviceDetails);
 
 
-    serviceDetails = new ServiceDetails().deserialize({label: 'Delete All (TODO)', rootObjectName : 'mycases',
-      operation : '/CreateCasesFromBordereaux',
+
+    serviceDetails = new ServiceDetails().deserialize({label: 'Delete All', rootObjectName : null,
+      operation : '/closeAllCase',
       apiUrl: 'https://eu-west-1.integration.cloud.tibcoapps.com/zwwupj46ttb7alnauy7exvxwihssu2y3'});
     this.serviceDetailsList.push(serviceDetails);
 
@@ -64,8 +71,32 @@ export class PdHomePageComponent implements OnInit {
   }
 
 
+
+  // On receive message from Select Service Display
   handleSelectedService (serviceDetails) {
     this.curServiceDetails = serviceDetails;
+
+    if (this.curServiceDetails.rootObjectName === null) {
+      const serviceObservable = this.serviceHandler.postService(serviceDetails.apiUrl + serviceDetails.operation, null);
+      serviceObservable.subscribe( result => {
+            this.openSnackBar(result);
+
+          },
+          error => {
+            this.openSnackBar(error);
+          });
+    }
+  }
+
+
+  // TODO refactor : it appears twice
+  openSnackBar(result: any) {
+    // TODO handle error
+    const message = 'File imported correctly : ' + result.nbTransmitted + ' lines transmitted';
+    const actionButtonLabel = 'Close';
+
+    this.snackBar.open( message, actionButtonLabel , {
+    });
   }
 
 }
