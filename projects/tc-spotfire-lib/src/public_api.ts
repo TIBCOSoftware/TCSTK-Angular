2dbb4379ed40c8679cfe5cafb5b4892103e3c066
jezrsmith <jez.smith@live.com>
2019-03-14 11:23:44 +0000

    Converted tc-spotfire-lib to be a real library integrated into source.

---
+++ projects/tc-spotfire-lib/src/public_api.ts
@@ -0,0 +1,19 @@
+/*
+ * Public API Surface of tc-liveapps-lib
+ */
+
+
+export * from './lib/tc-spotfire-lib.module';
+
+// components
+
+// data
+export * from './lib/models/tc-spotfire-config';
+
+// service
+export * from  './lib/services/tc-spotfire-config.service';
+
+// resolvers
+export * from './lib/resolvers/spotfire-config.resolver';
+
+// guards
