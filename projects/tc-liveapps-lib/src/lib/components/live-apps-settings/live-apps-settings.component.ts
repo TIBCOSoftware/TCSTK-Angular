9c0c07f0483533918aa5dacd7c439920a640ad29
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuels-MacBook-Pro.local>
2019-03-19 05:56:04 +0100

    Store liveapps configuration.

--- projects/tc-liveapps-lib/src/lib/components/live-apps-settings/live-apps-settings.component.ts
+++ projects/tc-liveapps-lib/src/lib/components/live-apps-settings/live-apps-settings.component.ts
@@ -2,34 +2,47 @@ import { Component, OnInit } from '@angular/core';
 import { LiveAppsConfig } from '../../models/tc-liveapps-config';
 import { ActivatedRoute } from '@angular/router';
 import { GeneralConfig } from 'tc-core-lib';
-import {Claim, Sandbox} from '../../models/liveappsdata';
+import { Claim, Sandbox } from '../../models/liveappsdata';
+import { TcLiveAppsConfigService } from '../../services/tc-live-apps-config.service';
 
 @Component({
-  selector: 'tcla-live-apps-settings',
-  templateUrl: './live-apps-settings.component.html',
-  styleUrls: ['./live-apps-settings.component.css']
+    selector: 'tcla-live-apps-settings',
+    templateUrl: './live-apps-settings.component.html',
+    styleUrls: ['./live-apps-settings.component.css']
 })
 export class LiveAppsSettingsComponent implements OnInit {
 
-  public liveAppsConfig: LiveAppsConfig;
-  public generalConfig: GeneralConfig;
-  public claims: Claim;
-  public sandbox: Sandbox;
-  public selectedAppIds: string[];
+    public sandboxId: number;
+    public selectedAppIds: string[];
+    public liveAppsConfig: LiveAppsConfig;
+    public generalConfig: GeneralConfig;
+    public claims: Claim;
 
-  constructor(private route: ActivatedRoute) { }
+    constructor(private route: ActivatedRoute, private liveAppsConfigService: TcLiveAppsConfigService) { }
 
-  public handleAppIdSelection(appIds: string[]) {
-    console.log('Selected: ', appIds);
-    this.selectedAppIds = appIds;
-  }
+    public handleAppIdSelection(appIds: string[]) {
+        console.log('Selected: ', appIds);
+        this.selectedAppIds = appIds;
+    }
 
-  ngOnInit() {
-    this.generalConfig = this.route.snapshot.data.laConfigHolder.generalConfig;
-    this.liveAppsConfig = this.route.snapshot.data.laConfigHolder.liveAppsConfig;
-    this.claims = this.route.snapshot.data.claims;
-    this.sandbox = this.claims.primaryProductionSandbox;
-    this.selectedAppIds = this.liveAppsConfig.applicationIds;
-  }
+    ngOnInit() {
+        this.generalConfig = this.route.snapshot.data.laConfigHolder.generalConfig;
+        this.liveAppsConfig = this.route.snapshot.data.laConfigHolder.liveAppsConfig;
+        this.claims = this.route.snapshot.data.claims;
+        this.sandboxId = Number(this.claims.primaryProductionSandbox.id).valueOf();
+        this.selectedAppIds = this.liveAppsConfig.applicationIds;
+    }
 
+    public runSaveFuntion = (): void => {
+
+        var liveAppsConfig = new LiveAppsConfig().deserialize({
+            applicationIds: this.selectedAppIds,
+            caseIconsFolderId: this.liveAppsConfig.caseIconsFolderId,
+            documentAppId: this.liveAppsConfig.documentAppId,
+            collaborationAppId: this.liveAppsConfig.documentAppId
+        });
+
+        this.liveAppsConfigService.updateLiveAppsConfig(this.sandboxId, "caseApp5", liveAppsConfig, this.liveAppsConfig.id).subscribe();
+    }
 }
+
