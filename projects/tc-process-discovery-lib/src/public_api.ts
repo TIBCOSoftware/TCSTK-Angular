8b740f4f5be34471dc3fcff90fbb3ff72efbd988
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuels-MacBook-Pro.local>
2019-03-20 16:17:36 +0100

    Created the PD Services and remove some old refs

--- projects/tc-process-discovery-lib/src/public_api.ts
+++ projects/tc-process-discovery-lib/src/public_api.ts
@@ -13,9 +13,12 @@ export * from './lib/components/pd-home/pd-home.component';
 export * from './lib/components/pd-process-mining/pd-process-mining.component';
 export * from './lib/components/pd-settings-administration/pd-settings-administration.component';
 export * from './lib/components/pd-settings-configuration/pd-settings-configuration.component';
-export * from './lib/components/pd-widget-header/pd-widget-header.component';
 export * from './lib/components/settings-integration/settings-integration.component';
 export * from './lib/components/settings-spotfire/settings-spotfire.component';
+
+// Services
+export * from './lib/services/pd-process-discovery-config.service';
+export * from './lib/services/pd-process-discovery.service';
 
 // Resolvers
 export * from './lib/resolvers/process-discovery-config.resolver';
