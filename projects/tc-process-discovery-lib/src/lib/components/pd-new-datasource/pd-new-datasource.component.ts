56dbd2ffac71d2c112e6af66078211e0adedd5ab
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuels-MacBook-Pro.local>
2019-03-24 08:01:12 +0100

    Create new datasource.

--- projects/tc-process-discovery-lib/src/lib/components/pd-new-datasource/pd-new-datasource.component.ts
+++ projects/tc-process-discovery-lib/src/lib/components/pd-new-datasource/pd-new-datasource.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { parse } from 'papaparse';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+
+
+export interface Mapping {
+    columnName: string,
+    options: string[]    
+}
 
 @Component({
     selector: 'tcpd-pd-new-datasource',
@@ -9,37 +16,105 @@ import { parse } from 'papaparse';
 export class PdNewDatasourceComponent implements OnInit {
 
     public file: File;
-    public useFirstRowAsHeader: boolean;
-    public columnSeparator: string;
-    public numberRowsForPreview: number;
+    public filename: string = '';
     public data;
+    public displayedColumns: string[];
+    public mapping: Mapping[] = [];
 
-    // public parseCharacter: string;
-    // public endString: string;
-    // public showPreview: boolean;
-    
+    // Options for parsing
+    public useFirstRowAsHeader: boolean = true;
+    public skipComments: boolean = false;
+    public comments: string = '//'
+    public columnSeparator: string;
+    public preview: boolean = true;
+    public numberRowsForPreview: number = 5;
+
     constructor() { }
 
     ngOnInit() {
-        this.numberRowsForPreview = 15;
     }
 
-    public onFileSelect = ($event: any): void => {
-        this.file = $event.files[0];
+    public calculateColumnNames = (numColumns: number, columnNames: string[]): string[] => {
+        let newColumnNames: string[] = [];
 
+        for (let index = 0; index < numColumns; index++){
+            const columnName = this.useFirstRowAsHeader ? columnNames[index] : '' + index;
+            const newElement: Mapping = {
+                columnName: columnName,
+                options: []
+            };
+            this.mapping.push(newElement);
+            newColumnNames.push(columnName);
+        }
+        return newColumnNames;
+    }
+
+    public onFileSelect = (fileList: File[]): void => {
+        this.file = fileList[0];
+        this.filename = this.file.name;
+        if (this.preview){
+            this.refresh();
+        }
+    }
+
+    public refresh = ():void =>{
         let config = {
+            header: this.useFirstRowAsHeader,
             preview: this.numberRowsForPreview,
+            comments: (this.skipComments) ? this.comments: '',
             complete: (result) => {
-                console.log("Lines: " + result.data.length);
+                console.log("***** In");
+                this.displayedColumns = this.calculateColumnNames(
+                    this.useFirstRowAsHeader ? Object.keys(result.data[0]).length: result.data[0].length, 
+                    this.useFirstRowAsHeader ? result.meta.fields: undefined);
                 this.data = result.data;
             }
         };
-
         parse(this.file, config);
-
-
-
- 
+    }
+    public setComments = ($event): void => {
+        if (!this.skipComments){
+            this.comments = '';
+        }
     }
 
+    icons = [
+        { id: 'Cancel', icon: 'close'},
+        { id: 'CaseId', icon: 'label'},
+        { id: 'Activity', icon: 'start'},
+        { id: 'Start', icon: 'timer'},
+        { id: 'End', icon: 'timer_off'},
+        { id: 'Resource', icon:'account_box'},
+        { id: 'Other', icon: 'swap_vert'}
+    ];
+
+    todo = [
+        'Cancel',
+        'Case ID',
+        'Activity',
+        'Start',
+        'End',
+        'Resource',
+        'Other'
+    ];
+
+    done = [
+        'Walk dog'
+    ];
+
+    drop(event: CdkDragDrop<string[]>) {
+        if (event.previousContainer === event.container) {
+            moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+        } else {
+            transferArrayItem(event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex);
+        }
+    }
+    onListDrop(event: CdkDragDrop<string[]>) {
+        // Swap the elements around
+        console.log("******* Drop");
+ //       moveItemInArray(this.myArray, event.previousIndex, event.currentIndex);
+    }
 }
