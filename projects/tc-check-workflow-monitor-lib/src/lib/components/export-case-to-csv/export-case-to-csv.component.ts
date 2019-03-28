696ba87ed02ee0d869298d5a9266831ceaeacd14
NamOsTibco <nthat@tibco.com>
2019-03-28 18:16:40 +0100

    fix

---
+++ projects/tc-check-workflow-monitor-lib/src/lib/components/export-case-to-csv/export-case-to-csv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tccwm-export-case-to-csv',
+  templateUrl: './export-case-to-csv.component.html',
+  styleUrls: ['./export-case-to-csv.component.css']
+})
+export class ExportCaseToCsvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
