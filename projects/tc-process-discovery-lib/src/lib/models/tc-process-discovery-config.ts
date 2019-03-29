3e602b5babeb069d419ff2ea69e8f21956d2cb15
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2019-03-29 13:23:38 +0100

    PD models and services for change datasource.

--- projects/tc-process-discovery-lib/src/lib/models/tc-process-discovery-config.ts
+++ projects/tc-process-discovery-lib/src/lib/models/tc-process-discovery-config.ts
@@ -5,6 +5,8 @@ export class ProcessDiscoveryConfig implements Deserializable {
     uiAppId: string;
     version: string;
     datasourceAppId: string;
+    creatorAppId: string;
+    locationToStoreFile: string;
     deserialize(input: any) {
         Object.assign(this, input);
         return this;
