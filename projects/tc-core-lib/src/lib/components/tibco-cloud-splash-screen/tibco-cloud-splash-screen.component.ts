15e0350f055f2fa5f0236c1cc013ca397c6c06c0
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuels-MacBook-Pro.local>
2019-03-31 08:03:58 +0200

    Make the splash screen more generic and expose button.

--- projects/tc-core-lib/src/lib/components/tibco-cloud-splash-screen/tibco-cloud-splash-screen.component.ts
+++ projects/tc-core-lib/src/lib/components/tibco-cloud-splash-screen/tibco-cloud-splash-screen.component.ts
@@ -1,53 +1,34 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {TcCoreCommonFunctions} from '../../common/tc-core-common-functions';
-import {Location} from '@angular/common';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { TcCoreCommonFunctions } from '../../common/tc-core-common-functions';
+import { Location } from '@angular/common';
 
 /**
  * @title Dialog Overview
  */
 @Component({
-  selector: 'tc-tibco-cloud-splash-screen',
-  templateUrl: 'tibco-cloud-splash-screen.component.html',
-  styleUrls: ['tibco-cloud-splash-screen.component.css'],
+    selector: 'tc-tibco-cloud-splash-screen',
+    templateUrl: 'tibco-cloud-splash-screen.component.html',
+    styleUrls: ['tibco-cloud-splash-screen.component.css'],
 })
 export class TibcoCloudSplashScreenComponent implements OnInit {
 
-  @Input() title: string;
-  @Input() subTitle: string;
-  @Input() backGroundImage: string;
-  @Input() pagePointsJSON: string;
-  public pagePoints = new Array();
+    @Input() title: string;
+    @Input() subTitle: string;
+    @Input() backGroundImage: string;
+    @Input() pagePointsJSON: string;
+    @Output() getStartedEvent: EventEmitter<string> = new EventEmitter<string>();
 
-  public pic_urls = new Array();
-  public loc;
-
-  public backGroundImageURL: string;
-
-  constructor(private location: Location) {
-    this.loc = location;
-  }
-
-  onStartedClick(): void {
-    // this.dialogRef.close();
-    console.log('Redirect to the home page.. ');
-  }
-
-
-  ngOnInit(): void {
-
-
-    console.log('JSON to Parse: ' + this.pagePointsJSON);
-    this.backGroundImageURL = TcCoreCommonFunctions.prepareUrlForStaticResource(this.loc, this.backGroundImage);
-    this.pagePoints = JSON.parse(this.pagePointsJSON);
-    for(var page of this.pagePoints){
-      console.log('URL ' + page.pic);
-      this.pic_urls.push(TcCoreCommonFunctions.prepareUrlForStaticResource(this.loc, page.pic));
+    public pagePoints = new Array();
+    constructor() {
     }
 
+    ngOnInit(): void {
+        this.pagePoints = JSON.parse(this.pagePointsJSON);
+    }
 
+    getStartedClick = (): void => {
+        this.getStartedEvent.emit('test');
+    }
 
-    console.log('Opening Splash Screen...');
-
-  }
 
 }
