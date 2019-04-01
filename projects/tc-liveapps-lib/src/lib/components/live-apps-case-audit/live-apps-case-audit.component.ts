254e9a507327dee64c760f50d665a84dc539b65c
jezrsmith <jez.smith@live.com>
2019-04-01 16:25:46 +0100

    Added audit using virtual scroll. Needs UX work.

--- projects/tc-liveapps-lib/src/lib/components/live-apps-case-audit/live-apps-case-audit.component.ts
+++ projects/tc-liveapps-lib/src/lib/components/live-apps-case-audit/live-apps-case-audit.component.ts
@@ -9,32 +9,58 @@ import {TcCaseAuditService} from '../../services/tc-case-audit.service';
   templateUrl: './live-apps-case-audit.component.html',
   styleUrls: ['./live-apps-case-audit.component.css']
 })
-export class LiveAppsCaseAuditComponent extends LiveAppsComponent implements OnInit, OnDestroy {
+export class LiveAppsCaseAuditComponent extends LiveAppsComponent implements OnDestroy {
 
   @Input() caseRef: string;
   @Input() sandboxId: number;
 
-  public auditEvents: AuditEvent[];
+  public auditEvents: AuditEvent[] = [];
   public errorMessage: string;
+  public startat = undefined;
+  public top = 20;
+  public end = false;
 
   public refresh = () => {
-    this.caseAuditService.getCaseAudit(this.caseRef, this.sandboxId, 0, 100)
+    this.startat = undefined;
+    this.top = 20;
+    this.end = false;
+    this.auditEvents = [];
+    this.getAuditEvents(this.caseRef, this.sandboxId, this.startat, this.top);
+  }
+
+  public getAuditEvents = (caseRef: string, sandboxId: number, startAt: number, top: number) => {
+    this.caseAuditService.getCaseAudit(this.caseRef, this.sandboxId, this.startat, this.top)
       .pipe(
         take(1),
         takeUntil(this._destroyed$),
         map(auditeventlist => {
-          this.auditEvents = auditeventlist.auditevents;
+          // this will strip any duplicates that may have been retrieved due to fast scrolling
+          const filteredEvents = auditeventlist.auditevents.filter(x => this.auditEvents.every(y => y.key.value !== x.key.value))
+          this.auditEvents = this.auditEvents.concat(filteredEvents);
+          if (auditeventlist.auditevents.length < this.top) {
+            this.end = true;
+          } else {
+            this.startat = auditeventlist.auditevents[auditeventlist.auditevents.length - 1].key.value;
+          }
         })
       ).subscribe(
-      null, error => { this.errorMessage = 'Error retrieving case audit: ' + error.error.errorMsg; });
+      null, error => {
+        this.errorMessage = 'Error retrieving case audit: ' + error.error.errorMsg;
+      });
+  }
+
+  public getNextBatch = (event) => {
+    if (!this.end) {
+      console.log('Request:', event);
+      console.log('skip:', this.startat);
+      console.log('top:', this.top);
+      console.log('end:', this.end);
+      this.getAuditEvents(this.caseRef, this.sandboxId, this.startat, this.top);
+    }
   }
 
   constructor(private caseAuditService: TcCaseAuditService) {
     super();
-  }
-
-  ngOnInit() {
-    this.refresh();
   }
 
 }
