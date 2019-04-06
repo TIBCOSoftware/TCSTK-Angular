99d7d8cbb47b0ebe2381e6571fae6a6451ad477e
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuels-MacBook-Pro.local>
2019-04-06 07:26:45 +0200

    Role switcher component.

---
+++ projects/tc-liveapps-lib/src/lib/components/live-apps-role-switcher/live-apps-role-switcher.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { RoleAttribute } from 'tc-core-lib';
+
+@Component({
+    selector: 'tcla-live-apps-role-switcher',
+    templateUrl: './live-apps-role-switcher.component.html',
+    styleUrls: ['./live-apps-role-switcher.component.css']
+})
+export class LiveAppsRoleSwitcherComponent implements OnInit {
+
+    @Input() roles: RoleAttribute[];
+    @Output() changeRoleEvent: EventEmitter<RoleAttribute> = new EventEmitter<RoleAttribute>();
+
+    public selectedRole: RoleAttribute;
+    constructor() { }
+
+    ngOnInit() {
+        console.log("Roles: ", this.roles);
+    }
+
+    public selectRole = (role) => {
+        this.changeRoleEvent.emit(role);
+    }
+
+}
