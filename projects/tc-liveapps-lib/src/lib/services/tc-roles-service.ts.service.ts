f90886dc41f8ecd43405a056b9cfe18dba769fc5
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2019-04-08 17:02:46 +0200

    Configurator function.

--- projects/tc-liveapps-lib/src/lib/services/tc-roles-service.ts.service.ts
+++ projects/tc-liveapps-lib/src/lib/services/tc-roles-service.ts.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
-import {LiveAppsService } from '../services/live-apps.service';
-import {Roles} from '../models/tc-groups-data';
-import {RolesResolver} from '../resolvers/roles.resolver';
-import {Router} from '@angular/router';
-import {HttpClient} from '@angular/common/http';
-import {Location} from '@angular/common';
-import {TcGeneralConfigService, TcSharedStateService} from 'tc-core-lib';
+import { LiveAppsService } from '../services/live-apps.service';
+import { Roles } from '../models/tc-groups-data';
+import { RolesResolver } from '../resolvers/roles.resolver';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { Location } from '@angular/common';
+import { TcGeneralConfigService, TcSharedStateService } from 'tc-core-lib';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class TcRolesService {
-  constructor() { }
+    
+    constructor() { }
 
-  private hasAccess = (roleReq: string, roles: Roles): boolean => {
-    const reqRole = roles.roles.find(role => {
-      return (role.id === roleReq);
-    })
-    return reqRole ? true : false;
-  }
+    private hasAccess = (roleReq: string, roles: Roles): boolean => {
+        const reqRole = roles.roles.find(role => {
+            return (role.id === roleReq);
+        })
+        return reqRole ? true : false;
+    }
 
-  checkRole(roleId: string, roles: Roles): boolean {
-    return this.hasAccess(roleId, roles);
-  }
+    checkRole(roleId: string, roles: Roles): boolean {
+        return this.hasAccess(roleId, roles);
+    }
+
+    amIConfigurator = (roles: Roles): boolean => {
+        const reqRole = roles.roles.find(role => {
+            return (role.configuration == true);
+        })
+        return reqRole ? true : false;
+    }
 }
