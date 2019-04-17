6e93e28aec3923ba067920750ff42f1961efa359
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2019-04-17 14:19:04 +0200

    PD Configuration model

--- projects/tc-process-discovery-lib/src/lib/models/tc-process-discovery-config.ts
+++ projects/tc-process-discovery-lib/src/lib/models/tc-process-discovery-config.ts
@@ -2,11 +2,19 @@ import { Deserializable } from 'tc-core-lib';
 
 export class ProcessDiscoveryConfig implements Deserializable {
     id: string;
-    uiAppId: string;
     version: string;
+    uiAppId: string;
     datasourceAppId: string;
     creatorAppId: string;
-    locationToStoreFile: string;
+    validateActionAppId: string;
+    storeToHDFS: boolean;
+    storeToLiveApps: boolean;
+    hdfsHostname: string;
+    hdfsRootPath: string;
+    hdfsUsername: string;
+    hdfsOverwriteFile: boolean;
+    hdfsPermision: string;
+    
     deserialize(input: any) {
         Object.assign(this, input);
         return this;
