be8b87fadf8e74b56c88c0ffeaaf2a17a32b3b34
jezrsmith <jez.smith@live.com>
2019-04-25 15:29:46 +0100

    Refactored App Routing to use different files for core vs starterApp route. + Handle PROVIDERS

---
+++ src/app/route-config/starter-app-route-config/configuration-route-config/configuration-route-config.ts
@@ -0,0 +1,79 @@
+import {ConfigurationMenuConfigResolver, GeneralConfigResolver, TibcoCloudSettingsGeneralComponent} from 'tc-core-lib';
+import {
+  AllGroupsResolver,
+  AllRolesResolver,
+  ClaimsResolver, GroupsResolver, LaConfigResolver, LiveAppsConfigResolver,
+  LiveAppsSettingsComponent, LiveAppsSettingsRecentCasesComponent,
+  LiveAppsSettingsRolesComponent, LiveAppsSettingsSummaryCardsComponent, RolesResolver
+} from 'tc-liveapps-lib';
+import {PdSettingsAdministrationComponent, PdSettingsConfigurationComponent} from 'tc-process-discovery-lib';
+import {ServiceDetailsConfigResolver, SettingsCwmServicesComponent} from 'tc-check-workflow-monitor-lib';
+import {SpotfireConfigResolver} from 'tc-spotfire-lib';
+
+export const CONFIGURATION_ROUTE_CONFIG = [
+  {
+    path: 'general-application-settings',
+    component: TibcoCloudSettingsGeneralComponent,
+    resolve: {
+      generalConfigHolder: GeneralConfigResolver,
+      claims: ClaimsResolver
+    }
+  },
+  {
+    path: 'general-application-roles',
+    component: LiveAppsSettingsRolesComponent,
+    resolve: {
+      generalConfigHolder: GeneralConfigResolver,
+      claims: ClaimsResolver,
+      allRoles: AllRolesResolver,
+      allGroups: AllGroupsResolver
+    }
+  },
+  {
+    path: 'live-apps-app-selection',
+    component: LiveAppsSettingsComponent,
+    resolve: {
+      claims: ClaimsResolver,
+      laConfigHolder: LaConfigResolver,
+      generalConfigHolder: GeneralConfigResolver
+    }
+  },
+  {
+    path: 'live-apps-recent-cases',
+    component: LiveAppsSettingsRecentCasesComponent,
+    resolve: {
+      claims: ClaimsResolver,
+      laConfigHolder: LaConfigResolver,
+      generalConfigHolder: GeneralConfigResolver
+    }
+  },
+  {
+    path: 'live-apps-summary-cards',
+    component: LiveAppsSettingsSummaryCardsComponent,
+    resolve: {
+      claims: ClaimsResolver,
+      laConfigHolder: LaConfigResolver,
+      generalConfigHolder: GeneralConfigResolver
+    }
+  },
+  {path: 'process-discovery-configuration', component: PdSettingsConfigurationComponent, resolve: {claims: ClaimsResolver}},
+  {path: 'process-discovery-administration', component: PdSettingsAdministrationComponent, resolve: {}},
+  {
+    path: 'upload-services-settings',
+    component: SettingsCwmServicesComponent,
+    resolve: {
+      serviceDetailsConfigResolver: ServiceDetailsConfigResolver
+    }
+  },
+  {
+    path: '**', redirectTo: '/starterApp/configuration/general-application-settings'
+  }
+];
+
+export const CONFIGURATION_ROUTE_PROVIDERS = [
+  GeneralConfigResolver,
+  ClaimsResolver,
+  AllRolesResolver,
+  AllGroupsResolver,
+  ServiceDetailsConfigResolver
+]
