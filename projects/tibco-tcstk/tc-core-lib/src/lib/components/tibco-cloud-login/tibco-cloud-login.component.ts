710b1ebd71e5a66aa2d55de6de0b18980e388d3a
Jörg Grote <joerg@grote-online.de>
2019-04-30 17:16:47 +0200

    adding combodocs

--- projects/tibco-tcstk/tc-core-lib/src/lib/components/tibco-cloud-login/tibco-cloud-login.component.ts
+++ projects/tibco-tcstk/tc-core-lib/src/lib/components/tibco-cloud-login/tibco-cloud-login.component.ts
@@ -1,24 +1,3 @@
-/**
- * @ngdoc component
- * @name tibcoCloudLoginComponent
- *
- * @description
- * `<tc-tibco-cloud-login-component>` is a component providing the ability to log into tibco cloud.
- *
- * @param {function callback} loggedIn Notify parent that user is logged in ok.
- * @param {function callback} subscriptionRequired Notify parent that user is in multiple subscriptions.
- *
- * @usage
- *
- * <tc-tibco-cloud-login *ngIf="!loggedIn && !subRequired" (loggedIn)="handleLoggedIn($event)"
- *    (subscriptionRequired)="handleSubscription($event)"></tc-tibco-cloud-login>
- *
- * This component will attempt to log the user in. If the user has multiple subscriptions then the
- * component will emit a list of available subscriptions. The user must select a subscription and
- * login to that subscription via the tibcoCloudSubscriptionComponent
- *
- */
-
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {AccountsInfo, LoginPrefill, Subscription} from '../../models/tc-login';
 import {Observable, ObservableInput} from 'rxjs';
@@ -26,6 +5,13 @@ import {map, mergeMap} from 'rxjs/operators';
 import {AccessToken, AuthInfo } from '../../models/tc-login';
 import {TcLoginService} from '../../services/tc-login.service';
 
+/**
+ * This component will attempt to log the user in. If the user has multiple subscriptions then the
+ * component will emit a list of available subscriptions. The user must select a subscription and
+ * login to that subscription via the tibcoCloudSubscriptionComponent
+ *
+ *  @example <tc-tibco-cloud-login *ngIf="!loggedIn && !subRequired" (loggedIn)="handleLoggedIn($event)" (subscriptionRequired)="handleSubscription($event)"></tc-tibco-cloud-login>
+ */
 @Component({
     selector: 'tc-tibco-cloud-login',
     templateUrl: './tibco-cloud-login.component.html',
@@ -34,7 +20,13 @@ import {TcLoginService} from '../../services/tc-login.service';
 
 export class TibcoCloudLoginComponent  implements OnInit {
 
+  /**
+   * Notify parent that user is logged in ok.
+   */
   @Output() loggedIn = new EventEmitter();
+  /**
+   * Login Data
+   */
   @Input() loginPrefill: LoginPrefill;
 
     name: string;
