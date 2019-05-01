10051b4d1ab8bf541982d6f7401aaddd2b983065
jezrsmith <jez.smith@live.com>
2019-05-01 15:38:26 +0100

    Re-work error handling

--- projects/tibco-tcstk/tc-core-lib/src/lib/components/tibco-cloud-error/tibco-cloud-error.component.ts
+++ projects/tibco-tcstk/tc-core-lib/src/lib/components/tibco-cloud-error/tibco-cloud-error.component.ts
@@ -14,55 +14,41 @@ export class TibcoCloudErrorComponent implements OnInit {
     this.route.params.subscribe(params => {
       console.log(params); // log the entire params object
       console.log(params['errorCode']);
-      console.log(params['errorMessage?']);
+      console.log(params['errorData?']);
       if (params['errorCode'] != null) {
         this.code = params['errorCode'];
+        this.title = this.knownErrorList.find(x => x.errorCode === this.code).errorTitle;
         this.message = this.knownErrorList.find(x => x.errorCode === this.code).errorMessage;
+        this.data = params['errorData'];
       } else {
         this.code = 'Unknown Error Code';
       }
-      if (this.message === '' && params['errorMessage?'] != null) {
-        this.message = params['errorMessage?'];
-      }
-
-      // log the value of id
     });
   }
 
   code: string;
+  title: string;
   message: string;
+  data: string;
 
   knownErrorList = [
     {
-      errorCode : 'NO_ROLE',
+      'errorCode' : 'NO_ROLE',
+      'errorTitle' : 'Access Denied',
       'errorMessage' : 'You are not a Member of this Application, please contact the Application- or Subscription-Owner.'
 
     },
     {
       'errorCode' : 'NO_ACCESS',
+      'errorTitle' : 'Access Denied',
       'errorMessage' : 'You are not entitled to access this Application, please contact the Application- or Subscription-Owner.'
 
     },
     {
       'errorCode' : 'NO_ROUTE_ACCESS',
-      'errorMessage' : 'You are not entitled to access this page of the application, please contact the Application- or Subscription-Owner.'
-
-    },
-    {
-      'errorCode' : 'CONFIGURATION_MISSING',
-      'errorMessage' : 'The Portal configuration seems wrong, this can only be fixed by an Application Configurator.'
-
-    },
-    {
-      'errorCode' : 'WRONG_APPLICATION_ID',
-      'errorMessage' : 'The linked LiveApps Application ID is invalid, this can only be fixed by an Application Configurator.'
-
-    },
-    {
-      'errorCode' : 'GROUP_ERROR' ,
-      'errorMessage' : 'There is a cache issue around groups, this can only be fixed by an Application Configurator.'
-
-    },
+      'errorTitle' : 'Access Denied',
+      'errorMessage' : 'Sorry but you do not have the required role to access this page of the application, please contact the application owner'
+    }
   ];
 
   ngOnInit() {
