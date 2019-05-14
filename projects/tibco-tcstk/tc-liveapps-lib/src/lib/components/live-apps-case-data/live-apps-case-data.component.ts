7c098a61436ac36c8db3c98776d4ddeceb505102
NamOsTibco <nthat@tibco.com>
2019-05-14 15:28:26 +0200

    comments

--- projects/tibco-tcstk/tc-liveapps-lib/src/lib/components/live-apps-case-data/live-apps-case-data.component.ts
+++ projects/tibco-tcstk/tc-liveapps-lib/src/lib/components/live-apps-case-data/live-apps-case-data.component.ts
@@ -7,20 +7,61 @@ import {LiveAppsComponent} from '../live-apps-component/live-apps-component.comp
 import {TcCaseDataService} from '../../services/tc-case-data.service';
 import {CustomFormDefs} from '@tibco-tcstk/tc-forms-lib';
 
+
+/**
+ * Displays data for a case in a widget (high level)
+ *
+ *@example <tcla-live-apps-case-data></tcla-live-apps-case-data>
+ */
 @Component({
   selector: 'tcla-live-apps-case-data',
   templateUrl: './live-apps-case-data.component.html',
   styleUrls: ['./live-apps-case-data.component.css']
 })
 export class LiveAppsCaseDataComponent extends LiveAppsComponent implements OnInit {
+  /**
+   * The case reference
+   */
   @Input() caseRef: string;
+
+  /**
+   * The LA Application Id
+   */
   @Input() appId: string;
+
+  /**
+   * The LA Application Type Id (generally 1)
+   */
   @Input() typeId: string;
+
+  /**
+   * sandboxId - this comes from claims resolver
+   */
   @Input() sandboxId: number;
+
+  /**
+   * The Application ID of the UI (should ideally be unique as it is shared state key)
+   */
   @Input() uiAppId: string;
+
+  /**
+   * Whether to show the header bar in the widget - eg. favorites on home page (contains icon etc) - if off icons still appear without bar
+   */
   @Input() showHeader: boolean;
+
+  /**
+   * Layout object that can be passed to override default layout of the form renderer
+   */
   @Input() layout: any[];
+
+  /**
+   * Custom Form configuration file
+   */
   @Input() customFormDefs: CustomFormDefs;
+
+  /**
+   * not used
+   */
   @Input() customDataId = this.customDataId ? this.customDataId : 'default';
 
   public casedata: any;
