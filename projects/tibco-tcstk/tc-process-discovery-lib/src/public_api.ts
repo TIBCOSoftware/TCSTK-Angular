a8304ba08aa5eb7ef171d796a966f5a2884f0080
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2019-05-20 09:39:31 +0200

    Clean default repo from tc-process-discovery-lib.

--- projects/tibco-tcstk/tc-process-discovery-lib/src/public_api.ts
+++ projects/tibco-tcstk/tc-process-discovery-lib/src/public_api.ts
@@ -5,24 +5,10 @@
 export * from './lib/tc-process-discovery-lib.module';
 
 // Components
-export * from './lib/components/pd-case-view/pd-case-view.component';
-export * from './lib/components/pd-new-datasource/pd-new-datasource.component';
-export * from './lib/components/pd-process-mining/pd-process-mining.component';
-export * from './lib/components/pd-settings-administration/pd-settings-administration.component';
-export * from './lib/components/pd-settings-configuration/pd-settings-configuration.component';
-export * from './lib/components/settings-integration/settings-integration.component';
-export * from './lib/components/proces-discovery-change-datasource-dialog/proces-discovery-change-datasource-dialog.component';
-export * from './lib/components/proces-discovery-change-datasource-dialog/proces-discovery-change-datasource-dialog.component';
-export * from './lib/components/pd-case/pd-case.component';
-
 
 // Services
-export * from './lib/services/pd-process-discovery-config.service';
-export * from './lib/services/pd-process-discovery.service';
 
 // Resolvers
-export * from './lib/resolvers/process-discovery-config.resolver';
 
 // Guards
-export * from './lib/guards/selectedRole.guard';
 
