231cc8f591298368ba5db39354a4392d849a2614
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@manuels-mbp.emea.tibco.com>
2019-06-27 12:09:41 +0200

    Active role resolver.

---
+++ projects/tibco-tcstk/tc-liveapps-lib/src/lib/resolvers/role-active.resolver.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRoute } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { LiveAppsService } from '../services/live-apps.service';
+import { GeneralConfigResolver, RoleAttribute, TcGeneralConfigService, TcSharedStateService } from '@tibco-tcstk/tc-core-lib';
+import { flatMap, map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Location } from '@angular/common';
+import { ClaimsResolver } from './claims.resolver';
+import { TcRolesService } from '../services/tc-roles-service.ts.service';
+
+@Injectable()
+export class RoleActiveResolver implements Resolve<Observable<RoleAttribute>> {
+
+    constructor(
+        private rolesService: TcRolesService, 
+        private liveAppsService: LiveAppsService, 
+        private route: ActivatedRoute,
+        private tcSharedStateService: TcSharedStateService,
+        private generalConfigService: TcGeneralConfigService, 
+        private httpClient: HttpClient,
+        private location: Location) {
+    }
+
+    resolve(): Observable<RoleAttribute> {
+
+        const currentRole = this.rolesService.getCurrentRole();
+
+        if (currentRole == undefined){
+            let generalConfigResolver = new GeneralConfigResolver(this.tcSharedStateService, this.generalConfigService, this.httpClient, this.location);
+            const claimResolver$ = new ClaimsResolver(this.liveAppsService);
+            return claimResolver$.resolve().pipe(
+                flatMap(claim => {
+                    const sandboxId = claim.primaryProductionSandbox.id;
+                    generalConfigResolver.setSandbox(Number(sandboxId));
+
+                    return generalConfigResolver.resolve().pipe(
+                        map(generalConfig => {
+                            const highestRole = generalConfig.roles.reduce(function (prev, current) {
+                                return (prev.priority > current.priority) ? prev : current
+                            });
+                            this.rolesService.setCurrentRole(highestRole);
+                            return highestRole;                            
+                        })
+                    );
+                })
+            );
+         }else {
+            return of(currentRole);            
+        }
+    }
+}
