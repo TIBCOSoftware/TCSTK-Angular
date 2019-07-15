56543e42a69dc698c931722d90d52020bdf61f02
jezrsmith <jez.smith@live.com>
2019-07-15 16:25:13 +0100

    Added showcase for testing widget responsiveness

--- src/app/routes/showcase/showcase.component.spec.ts
+++ src/app/routes/showcase/showcase.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SplashPDComponent } from './splash-pd.component';
+import { ShowcaseComponent } from './showcase.component';
 
-describe('SplashPDComponent', () => {
-  let component: SplashPDComponent;
-  let fixture: ComponentFixture<SplashPDComponent>;
+describe('ShowcaseComponent', () => {
+  let component: ShowcaseComponent;
+  let fixture: ComponentFixture<ShowcaseComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SplashPDComponent ]
+      declarations: [ ShowcaseComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SplashPDComponent);
+    fixture = TestBed.createComponent(ShowcaseComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
---
+++ src/app/routes/showcase/showcase.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {Claim, GeneralConfig} from '@tibco-tcstk/tc-core-lib';
+import {CaseType, LiveAppsConfig} from '@tibco-tcstk/tc-liveapps-lib';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+  selector: 'laapp-showcase',
+  templateUrl: './showcase.component.html',
+  styleUrls: ['./showcase.component.css']
+})
+export class ShowcaseComponent implements OnInit {
+  public generalConfig: GeneralConfig;
+  public liveAppsConfig: LiveAppsConfig;
+  public sandboxId: number;
+  private claims: Claim;
+  public selectedAppConfig: CaseType;
+  public userName: string;
+  public userId: string;
+  public email: string;
+  public widgetSize = 100;
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  toggleWidgetSize = () => {
+    if (this.widgetSize === 100) {
+      this.widgetSize = 75;
+    } else if (this.widgetSize === 75) {
+      this.widgetSize = 50;
+    } else if (this.widgetSize === 50) {
+      this.widgetSize = 33;
+    } else {
+      this.widgetSize = 100;
+    }
+  }
+
+  ngOnInit() {
+    this.generalConfig = this.route.snapshot.data.laConfigHolder.generalConfig;
+    this.liveAppsConfig = this.route.snapshot.data.laConfigHolder.liveAppsConfig;
+    this.claims = this.route.snapshot.data.claims;
+    this.sandboxId = this.route.snapshot.data.claims.primaryProductionSandbox.id;
+    this.userName = this.claims.firstName + ' ' + this.claims.lastName;
+    this.email = this.claims.email;
+    this.userId = this.claims.id;
+  }
+
+}
