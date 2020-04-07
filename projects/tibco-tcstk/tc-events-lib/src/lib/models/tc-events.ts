942f314a96fef2b38670c53ccb3acd59360c4ef1
hpeters@tibco.com <hpeters@tibco.com>
2020-04-07 10:06:34 +0200

    tc events model

---
+++ projects/tibco-tcstk/tc-events-lib/src/lib/models/tc-events.ts
@@ -0,0 +1,77 @@
+/* Models used to store Events Interactions */
+
+import {Deserializable} from '@tibco-tcstk/tc-core-lib';
+
+export class EventsAttribute {
+  Id: string;
+  type: string;
+}
+
+export class EventsRecordHolder {
+  attributes: EventsRecord;
+  apiToken: string;
+  isDMInstalled: boolean;
+  isLockingEnabled: boolean;
+  isBPMNInstalled: boolean;
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  subscriptionId: string;
+}
+
+export class EventsRecord {
+  attributes: EventsAttribute;
+}
+
+export class EventsData {
+  attributes: EventsAttribute;
+  record: EventsRecordHolder[];
+}
+
+export class EventsResponseMessage {
+  response: EventsResponse;
+}
+
+export class EventsResponse implements Deserializable {
+  attributes: EventsAttribute;
+  status: string;
+  startRow: number;
+  endRow: number;
+  totalRows: number;
+  data: EventsData;
+  responseMessage: string;
+  errorCode: string;
+  deserialize(input: any) {
+    Object.assign(this, input);
+    return this;
+  }
+}
+
+export class CheckoutArtifact {
+  artifactPath: string;
+  artifactType: string;
+  fileExtension: string;
+  baseArtifactPath: string;
+}
+
+export class EventsProject {
+  name: string;
+  artifactItem: CheckoutArtifact[];
+}
+
+export class EventsReqData {
+  project: EventsProject;
+}
+
+export class EventsRequest {
+  data: EventsReqData;
+}
+
+export class EventsRequestMessage implements Deserializable {
+  request: EventsRequest;
+  deserialize(input: any) {
+    Object.assign(this, input);
+    return this;
+  }
+}
