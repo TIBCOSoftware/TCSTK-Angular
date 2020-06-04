33dd4049be0b8a53cd71c72a77392bf35df689b8
Manuel Carlos Rodríguez Álvarez-Querol <mcrodrig@Manuel-Carlos-Rodriguezs-MacBook-Pro.local>
2020-06-04 12:15:18 +0200

    Recent and favorite components with table.

---
+++ projects/tibco-tcstk/tc-primeng-lib/src/lib/components/tcprimeng-favorite-cases/tcprimeng-favorite-cases.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { LiveAppsFavoriteCasesComponent } from '@tibco-tcstk/tc-liveapps-lib';
+
+@Component({
+  selector: 'tcpmg-tcprimeng-favorite-cases',
+  templateUrl: './tcprimeng-favorite-cases.component.html',
+  styleUrls: ['./tcprimeng-favorite-cases.component.css']
+})
+export class TcprimengFavoriteCasesComponent extends LiveAppsFavoriteCasesComponent  {
+
+}
--- projects/tibco-tcstk/tc-primeng-lib/src/lib/tc-primeng-lib.module.ts
+++ projects/tibco-tcstk/tc-primeng-lib/src/lib/tc-primeng-lib.module.ts
@@ -2,22 +2,34 @@ import { NgModule } from '@angular/core';
 import { TcPrimengLibComponent } from './tc-primeng-lib.component';
 import { TcprimengLiveappsCasesComponent } from './components/tcprimeng-liveapps-cases/tcprimeng-liveapps-cases.component';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 import { TableModule } from 'primeng/table';
 import { DropdownModule } from 'primeng/dropdown';
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { TcprimengRecentCasesComponent } from './components/tcprimeng-recent-cases/tcprimeng-recent-cases.component';
+import { TcLiveappsLibModule } from '@tibco-tcstk/tc-liveapps-lib';
+import { TcprimengFavoriteCasesComponent } from './components/tcprimeng-favorite-cases/tcprimeng-favorite-cases.component';
 
 @NgModule({
   declarations: [
     TcPrimengLibComponent,
-    TcprimengLiveappsCasesComponent
+    TcprimengLiveappsCasesComponent,
+    TcprimengRecentCasesComponent,
+    TcprimengFavoriteCasesComponent
   ],
   imports: [
+    TcLiveappsLibModule,
     CommonModule,
     TableModule,
-    DropdownModule
+    DropdownModule,
+    FlexLayoutModule,
+    MatIconModule
   ],
   exports: [
     TcPrimengLibComponent,
-    TcprimengLiveappsCasesComponent
+    TcprimengLiveappsCasesComponent,
+    TcprimengRecentCasesComponent,
+    TcprimengFavoriteCasesComponent
   ]
 })
 export class TcPrimengLibModule { }
