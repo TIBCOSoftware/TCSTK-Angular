60f14057bb1f6e9b7f082ba8dcdb864146f50ceb
mcrodrig <mcrodrig@tibco.com>
2020-09-03 12:46:14 +0200

    Use input/output parameters instead of resolvers

--- projects/tibco-tcstk/tc-spotfire-lib/src/lib/components/settings-spotfire/settings-spotfire.component.ts
+++ projects/tibco-tcstk/tc-spotfire-lib/src/lib/components/settings-spotfire/settings-spotfire.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { TcSpotfireConfigService } from '../../services/tc-spotfire-config.service';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { SpotfireConfig } from '../../models/tc-spotfire-config';
 
 @Component({
@@ -11,82 +8,24 @@ import { SpotfireConfig } from '../../models/tc-spotfire-config';
 })
 export class SettingsSpotfireComponent implements OnInit {
 
-    public spotfireServer: string;
-    public analysisPath: string;
-    public tableName: string;
-    public activePageForHome: string;
-    public activePageForDetails: string;
-    public markingName: string;
-    public maxMarkings: number;
-    public allowedPages: string;
-    public columnNames: string;
-    private id: string;
-    private uiAppId: string;
-    private sandboxId: number;
+    @Input() spotfireConfig: SpotfireConfig;
 
-    constructor(
-      protected route: ActivatedRoute,
-      protected spotfireConfigService: TcSpotfireConfigService,
-      protected snackBar: MatSnackBar
-    ) { }
+    @Output() handleSave: EventEmitter<void> = new EventEmitter();
+
+    constructor() { }
 
     ngOnInit() {
-        this.refresh();
     }
 
-    private refresh = (): void => {
-        const spotfireConfig = this.route.snapshot.data.spotfireConfigHolder;
-        this.sandboxId = this.route.snapshot.data.claimsHolder.primaryProductionSandbox.id;
-
-        this.spotfireServer = spotfireConfig.spotfireServer;
-        this.analysisPath = spotfireConfig.analysisPath;
-
-        this.tableName = spotfireConfig.tableName;
-
-        this.activePageForHome = spotfireConfig.activePageForHome;
-        this.activePageForDetails = spotfireConfig.activePageForDetails;
-
-        this.markingName = spotfireConfig.markingName;
-        this.maxMarkings = spotfireConfig.maxMarkings;
-
-        this.allowedPages = spotfireConfig.allowedPages.join(',');
-        this.columnNames = spotfireConfig.columnNames.join(',');
-
-        this.id = spotfireConfig.id;
-        this.uiAppId = spotfireConfig.uiAppId;
-    }
-
-    public runSaveFunction = (): void => {
-        const spotfireConfig = new SpotfireConfig().deserialize({
-            id: this.id,
-            uiAppId: this.uiAppId,
-            spotfireServer: this.spotfireServer,
-            analysisPath: this.analysisPath,
-            tableName: this.tableName,
-            activePageForHome: this.activePageForHome,
-            activePageForDetails: this.activePageForDetails,
-            markingName: this.markingName,
-            maxMarkings: this.maxMarkings,
-            allowedPages: this.allowedPages.split(','),
-            columnNames: this.columnNames.split(',')
-        });
-
-        this.spotfireConfigService.updateSpotfireConfig(this.sandboxId, this.uiAppId, spotfireConfig, this.id).subscribe(
-            result => {
-                this.snackBar.open('Spotfire settings saved', 'OK', {
-                    duration: 3000
-                });
-            },
-            error => {
-                this.snackBar.open('Error saving Spotfire settings saved', 'OK', {
-                    duration: 3000
-                });
-            }
-        );
+    /**
+     * Save Button
+     */
+    runSave(){
+        this.handleSave.emit();
     }
 
     public handleEditLiveAppClick = (): void => {
-        window.open(this.spotfireServer + '/spotfire/wp/analysis?file=' + this.analysisPath);
+        window.open(this.spotfireConfig.spotfireServer + '/spotfire/wp/analysis?file=' + this.spotfireConfig.analysisPath);
     }
 
 }
