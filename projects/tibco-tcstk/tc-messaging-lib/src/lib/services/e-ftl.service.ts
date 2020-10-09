ad78eeec78b3736511edf6e3dc10cfe95a71c65e
jezrsmith <jez.smith@live.com>
2020-10-09 15:29:23 +0100

    Commented out uxpl spinner

--- projects/tibco-tcstk/tc-messaging-lib/src/lib/services/e-ftl.service.ts
+++ projects/tibco-tcstk/tc-messaging-lib/src/lib/services/e-ftl.service.ts
@@ -14,49 +14,51 @@ export class EFTLService {
   }
 
   public connect = (wssUrl: string, apiKey: string, clientId: string): Observable<any> => {
-    const sendResult = new Subject<any>();
-    eFTL.connect(wssUrl, {
-        password: apiKey,
-        clientId: clientId,
-        onConnect: (connection) => {
-          this.connection = connection;
-          sendResult.next('Connected');
-        },
-        onDisconnect: (connection, code, reason) => {
-          console.error('eFTL: connection lost, ' + reason);
-          throwError({ connection: connection, code: code, reason: reason });
-          this.connection = undefined;
+    const sendResult = new Observable<any>((observer) => {
+      eFTL.connect(wssUrl, {
+          password: apiKey,
+          clientId: clientId,
+          onConnect: (connection) => {
+            this.connection = connection;
+            observer.next('Connected');
+          },
+          onDisconnect: (connection, code, reason) => {
+            console.error('eFTL: connection lost, ' + reason);
+            observer.error({connection: connection, code: code, reason: reason});
+          }
         }
-      }
-    );
-    return sendResult.asObservable();
+      );
+    });
+    return sendResult;
   }
 
   public sendMessage = (message: string, event: string, dest?: string): Observable<any> => {
-    const sendResult = new Subject<any>();
-    if (this.connection) {
-      // setup the message
-      const msg = this.connection.createMessage();
-      if (dest) {
-        msg.set('_dest', dest);
-      }
-      msg.set('event', event);
-      msg.set('text', message);
-
-      // publish a message to TIBCO Cloud Messaging
-      this.connection.publish(msg, {
-        onComplete: (response) => {
-          sendResult.next(response);
-        },
-        onError: (response, code, reason) => {
-          console.error('eFTL: unable to send message:', response);
-          throwError({ response: response, code: code, reason: reason });
+    // const sendResult = new Subject<any>();
+    const sendResult = new Observable<any>((observer) => {
+      if (this.connection) {
+        // setup the message
+        const msg = this.connection.createMessage();
+        if (dest) {
+          msg.set('_dest', dest);
         }
-      });
-    } else {
-      throwError('eFTL: No connection to eFtl');
-    }
-    return sendResult.asObservable();
+        msg.set('event', event);
+        msg.set('text', message);
+
+        // publish a message to TIBCO Cloud Messaging
+        this.connection.publish(msg, {
+          onComplete: (response) => {
+            observer.next(response);
+          },
+          onError: (response, code, reason) => {
+            console.error('eFTL: unable to send message:', response);
+            observer.error({ response: response, code: code, reason: reason });
+          }
+        });
+      } else {
+        throwError('eFTL: No connection to eFtl');
+      }
+    });
+    return sendResult;
   }
 
   public receiveMessage = (matcher: string, durable: string): Observable<any> => {
